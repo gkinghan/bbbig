002fdac8329a525d13c055997c069866ebe550a0
hange <849414545@qq.com>
2020-09-14 17:00:38 +0800

    文章列表

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -52,6 +52,73 @@ $(function() {
     }
 
     // 筛选按钮
+    $('#form-search').submit(function(e) {
+        e.preventDefault()
 
+        q.cate_id = $('[name=cate_id]').val()
+        q.state = $('[name=state]').val()
+            // console.log(q)
+        initList()
+    })
+
+    // 渲染分页
+
+    function renderPage(total) {
+        // 分页代码
+        layui.use('laypage', function() {
+            var laypage = layui.laypage
+
+            //执行一个laypage实例
+            laypage.render({
+                elem: 'page', //注意，这里的 test1 是 ID，不用加 # 号
+                count: total, //数据总数，从服务端得到
+                curr: q.pagenum, // 当前页码
+                limit: q.pagesize, //  每页条数
+
+                limits: [2, 3, 5, 10], // 切换每页条数pagesize
+                layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+
+                // 情况1：默认第一次时调用！！！！！！！
+                // 情况2：切换页码时，调用jump函数
+                jump: function(obj, first) {
+                    //首次不执行
+                    if (!first) {
+                        //obj包含了当前分页的所有参数，比如：
+                        // console.log(obj.curr) //得到当前页，以便向服务端请求对应页的数据。
+                        // console.log(obj.limit); //得到每页显示的条数
+
+                        // 按照最新页码获取文章列表数据
+                        q.pagenum = obj.curr // 2  pagesize=2  => 服务器返回数据库中的第3和第4条数据
+                        q.pagesize = obj.limit
+                            //do something
+                        initList()
+                    } else {}
+                },
+            })
+        })
+    }
+
+    // 删除分类
+    $('tbody').on('click', '.delete', function(e) {
+        e.preventDefault()
+            // 获取id
+        var Id = $(this).attr('data-id')
+        var len = $('.delete').length
+
+        // 确认框
+        layer.confirm('Sure?', { icon: 3, title: '删除文章' }, function(index) {
+            // ajax
+            $.get(`/my/article/delete/${Id}`, function(res) {
+                if (res.status === 0) {
+                    if (len === 1) {
+                        // 改变页码pagenum
+                        q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
+                    }
+                    layer.close(index)
+                    initList()
+                }
+            })
+        })
+    })
 
 })
